096dda235f7c3f8590f078c5b083fd8938fb77dd
Paul Carey <paul.carey@ignition.it>
2014-06-02 11:57:22 +0100

    Increase download timeout to half an hour

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,7 @@ class androidbundle() {
     checksum => false,
     url    => 'http://dl.google.com/android/adt/22.6.2/adt-bundle-linux-x86_64-20140321.zip',
     target => '/opt/adt',
+    timeout => 1800,
     require => Package["unzip"]
   }
 
